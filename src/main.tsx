3ab2fa18fe4003e7ef14fc2f46913ceda463c32d
vamaspyp <vamaspyp@gmail.com>
2026-09-14 14:26:54 -0300

    A63 V19 E&S: activate master scene layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './worldclass.css'
 import App from './App.tsx'
+import './master-scenes.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
