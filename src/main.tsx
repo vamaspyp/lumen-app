9d728eaa2e27022f25d697b1b109bef3fc91b5ca
vamaspyp <vamaspyp@gmail.com>
2026-09-15 18:17:13 -0300

    A63: expose certification atlas without parallel product runtime

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppE63 from './AppE63'
+import ExperienceAtlasRoute from './ExperienceAtlasRoute'
+
+const certificationAtlas = window.location.pathname === '/experience-atlas' || window.location.pathname === '/experience-atlas/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppE63 />
+    {certificationAtlas ? <ExperienceAtlasRoute /> : <AppE63 />}
   </StrictMode>,
 )
