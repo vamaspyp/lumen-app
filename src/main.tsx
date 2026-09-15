63d953aa958b31b3df453f5a746c5c253fddaed8
vamaspyp <vamaspyp@gmail.com>
2026-09-15 10:04:29 -0300

    A63 E&S: wire true React master route

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './worldclass.css'
 import App from './App.tsx'
+import MasterReact from './master-react/MasterReact'
 import './master-scenes.css'
 import './master-photographic.css'
 
+const isMasterReact = window.location.pathname === '/master-react' || window.location.pathname === '/master-react/'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isMasterReact ? <MasterReact /> : <App />}
   </StrictMode>,
 )
