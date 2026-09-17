c4898084c2fefc60acaf43f2f707497c5d22b0f4
vamaspyp <vamaspyp@gmail.com>
2026-09-17 16:33:15 -0300

    feat(a63): mount continuous LUMI and premium field v4

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './app/styles.css'
 import './app/a63-continuity.css'
 import './app/accessibility.css'
+import './app/premium-v4.css'
 import App from './app/App'
+import { LumiPresence } from './app/LumiPresence'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <LumiPresence />
   </StrictMode>,
 )
