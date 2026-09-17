a9f178314bf6a81af3d8b93730320dfacc2cb8a2
vamaspyp <vamaspyp@gmail.com>
2026-09-17 15:04:04 -0300

    style(a63): load continuity field styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/styles.css'
+import './app/a63-continuity.css'
 import './app/accessibility.css'
 import App from './app/App'
 
