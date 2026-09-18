8a077b0f391a9b7dac2a3d4695a32d3b94bb1803
vamaspyp <vamaspyp@gmail.com>
2026-09-18 10:45:19 -0300

    style(a63): mount premium Source constellation field

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './app/premium-v4.css'
 import './app/premium-v4-ci.css'
 import './app/premium-v4-feedback.css'
 import './app/a63-source-consistency.css'
+import './app/premium-source.css'
 import App from './app/App'
 import { LumiPresence } from './app/LumiPresence'
 
