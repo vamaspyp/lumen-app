d924a314a73983a48da839b37f63232c500bab41
vamaspyp <vamaspyp@gmail.com>
2026-09-18 12:10:48 -0300

    A64: load premium unified layer after legacy visual CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './premium-field.css'
+import './premium-unified.css'
 import AppE63 from './AppE63'
 import ExperienceAtlasRoute from './ExperienceAtlasRoute'
 
